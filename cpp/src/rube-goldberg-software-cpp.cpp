b432f67c64f32eb89ed8a6191c37d0a1aa327151
MarkusSprunck <markus.sprunck@mnet-online.de>
2016-05-16 11:48:55 +0200

    Add timestamp to cpp log

--- cpp/src/rube-goldberg-software-cpp.cpp
+++ cpp/src/rube-goldberg-software-cpp.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <ctime>
+#include <sys/time.h>
 
 using namespace std;
 
+std::string getTime() {
+	timeval curTime;
+	time_t now;
+
+	time(&now);
+	gettimeofday(&curTime, NULL);
+
+	int milli = curTime.tv_usec / 1000;
+	char buf[sizeof "2011-10-08T07:07:09"];
+	strftime(buf, sizeof buf, "%FT%T", gmtime(&now));
+	sprintf(buf, "%s.%d", buf, milli);
+
+	return buf;
+}
+
 int main() {
-	cout << "Hello, from C++" << endl; // prints Hello, from C++
-	usleep(1000000u);
-	cout << "Hello, from C++" << endl; // prints Hello, from C++
 
 	std::ofstream outfile;
-  	outfile.open("rube-goldberg-software.log", std::ios_base::app);
-  	outfile << "Hello, from C++" << endl;
+	outfile.open("rube-goldberg-software.log", std::ios_base::app);
+
+	outfile << getTime() << " - cpp - Hello, from C++ #1" << endl;
+	usleep(2000000u);
+
+	outfile << getTime() << " - cpp - Hello, from C++ #2" << endl;
 
 	return 0;
 }
